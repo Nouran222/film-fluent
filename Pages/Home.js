7a687dde0a4384a8b471906af1c20e6617ab95d0
Nouran222 <nouransayednasr@gmail.com>
2024-05-16 23:42:12 +0300

    Add movie data to movie details screen (closes #1)

--- Pages/Home.js
+++ Pages/Home.js
@@ -1,33 +1,52 @@
-import React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-
-const Home = ({navigation}) => {
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import routes from '../Utilies/routes';
+const Home = ({ navigation }) => {
     const imgPath = "https://image.tmdb.org/t/p/w500/";
     //useEffect(callback func,array of dependency)
     //useState to render the component once the data is recieved from the API
-    const [movies,setMovies] = useState([]);
-    useEffect(()=>{
-        fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9813ce01a72ca1bd2ae25f091898b1c7")
-        .then((res)=>res.json())
-        .then((data)=>setMovies(data.results))
-    },[]);
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9813ce01a72ca1bd2ae25f091898b1c7")
+            .then((res) => setMovies(res.data.results));
+        // fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9813ce01a72ca1bd2ae25f091898b1c7")
+        // .then((res)=>res.json())
+        // .then((data)=>setMovies(data.results))
+    }, []);
+
+    const {navigate} = useNavigation();
+
+    const handleImagePress = (movie)=>{
+        navigate(routes.details,movie)
+    }
+
+    const renderItem =  ({item})=>(
+        <TouchableOpacity onPress={()=>handleImagePress(item)}>
+                    <Image
+                        source={{
+                            uri: `${imgPath}${item.poster_path}`,
+                            width: 200,
+                            height: 300,
+                        }}
+                    ></Image>
+                </TouchableOpacity>
+    );
     return (
         //we can use useNavigation instead of the navigation prop
         // const {navigate} = useNavigation();
         //onPress={()=>{navigate("movies")}}
+
+        //we can send the movie id as a parameter in the navigate
+        //then we will recieve the parameter in the details page
         <View>
-            <Text>Movies Page</Text>
-            {movies.map((movie)=>
-            <Image
-            source={{
-                uri:`${imgPath}${movie.poster_path}`,
-                width:100,
-                height:100,
-            }}
-            key={movie.id}
-            ></Image>)}
+            <FlatList
+                data={movies}
+                renderItem={renderItem}
+                keyExtractor={(item)=>item.id.toString()}
+            ></FlatList>
         </View>
     );
 }
--- Pages/MovieDetails.js
+++ Pages/MovieDetails.js
@@ -1,10 +1,23 @@
+import { useRoute } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 const MovieDetails = () => {
+    const imgPath = "https://image.tmdb.org/t/p/w500/";
+
+    //recieve the id from the parameters
+    const {params:{poster_path,title,overview}} = useRoute();
+    // console.warn(params);
+
     return (
         <View>
-            <Text>Movie Details</Text>
+            <Image source={{
+                uri:`${imgPath}${poster_path}`,
+                width:200,
+                height:300
+            }}></Image>
+            <Text>title: {title}</Text>
+            <Text>overview: {overview}</Text>
         </View>
     );
 }
